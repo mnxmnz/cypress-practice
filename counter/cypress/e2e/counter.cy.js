c1e3ab2d36b68e0dfdf0fc471c03264910eca308
mnxmnz <minzidev@gmail.com>
2023-01-02 16:28:17 +0900

    ✅ test: counter 초기화 테스트 생성

--- counter/cypress/e2e/counter.cy.js
+++ counter/cypress/e2e/counter.cy.js
@@ -44,4 +44,10 @@ describe('Counter', () => {
     cy.get('.decrease-btn').click();
     cy.get('#value').invoke('text').should('eq', '0');
   });
+
+  it('Reset 버튼을 클릭했을 때 카운터 값이 0으로 초기화된다.', () => {
+    cy.get('.increase-btn').click();
+    cy.get('.reset-btn').click();
+    cy.get('#value').invoke('text').should('eq', '0');
+  });
 });
